c7d7f4490f4370009aa35433554d828a7d886636
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-01 13:12:34 -0800

    adjust dataset for todo list

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'make lasagna',
+    complete: false,
+  },
+  {
+    todo: 'make more lasagna',
+    complete: false,
+  },
+  {
+    todo: 'make even more lasagna',
+    complete: false,
+  }
+];
